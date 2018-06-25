86bea3097f24eb0d8c100c86cf0b89930f3526f8
jiwenyu <jiwenyu0531@qq.com>
2018-06-25 20:52:19 +0800

    update the DEBUG to false and generate the requirements.txt

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y*1lb3#08=9##evxcvb^urtt8v#tt0lew$wdq5=agk7#+s!y6g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '120.78.152.125']
 
 
 # Application definition
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
